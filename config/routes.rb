e87b7c5bcf30e6dbd4d2d663cbb873932b1d9249
victor-zacchi <victorzacchi@gmail.com>
2015-03-15 17:27:25 +0000

    Devise Login complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
+  #get '/login', :controller=> '/users/sessions', :action=>'create'
 
   resources :kuizuws
+
+  # devise_scope :users do
+  #   get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  #   devise_for :users
+  # end
+  
+  resources :users
+
 
   root :to => 'home#index'
 
